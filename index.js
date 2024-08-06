f7874059dd66827950d6851a703f078c3a5c3264
Agus <agustinguevara1949@gmail.com>
2024-08-05 23:33:59 -0300

    listo

---
+++ index.js
@@ -0,0 +1,92 @@
+//Actividad 1
+/* function esParOImpar(numero) {
+    if (numero % 2 === 0) {
+      console.log(`El número ${numero} es par.`);
+    } else {
+      console.log(`El número ${numero} es impar.`);
+    }
+  }
+  
+ esParOImpar(48) */
+
+ //////
+
+ //Actividad 2
+
+/*  function MayorOmenor(a, b) {
+    if (a > b) {
+      console.log(`El número ${a} es mayor que ${b}`);
+    } else if (a < b) {
+      console.log(`El número ${b} es mayor que ${a}`);
+    } else {
+      console.log(`Los números ${a} y ${b} son iguales`);
+    }
+  }
+  
+MayorOmenor(1000, 1000) */
+
+//Actividad 3
+
+/* function multiplo (num1){
+if(num1 % 5 ===0){
+    console.log(`El número ${num1} es múltiplo de 5`)
+}else{
+    console.log(`el numero ${num1} no es multiplo de 5`)
+}
+}
+multiplo(80) */
+
+// Actividad 4
+
+/* function todosLosNumeros (num1){
+ 
+   for( let i=0; i<=num1; i++){
+    console.log(i)
+   }
+}
+   todosLosNumeros()
+ */
+
+//Actividad 5
+
+/* function consigna(palabra, num1){
+    for (let i=0; i<num1; i++){
+        console.log(palabra)
+    }
+}
+consigna("agua", 9); */
+
+// Actividad 6
+
+/* function imprimirArray(arr) {
+    for (let i = 0; i < arr.length; i++) {
+      console.log(arr[i]);
+    }
+  }
+  
+  let frutas = ["Manzana", "Banana", "Naranja", "Kiwi", 2,3,4];
+  imprimirArray(frutas); */
+
+  // Actividad 7
+/*   function Array5(arr) {
+    for (let i = 0; i < arr.length; i++) {
+      if (i !== 4) { 
+        console.log(arr[i]);
+      }
+    }
+  }
+  
+ 
+  let numeros = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17];
+  Array5(numeros); */
+
+/*   function multiplicarNumeros(arr, num) {
+    for (let i = 0; i < arr.length; i++) {
+      console.log(arr[i] * num);
+    }
+  }
+  
+ 
+  let numeros = [1, 2, 3, 4, 5];
+  let multiplicador = 2;
+  multiplicarNumeros(numeros, multiplicador); */
